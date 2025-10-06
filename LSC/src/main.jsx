803f914803e1da9120c15685abd5cb7524f254f4
Thandolwethu Mbokazi <thandwem045@gmail.com>
2025-10-06 07:30:28 +0000

    Styled navigation

--- LSC/src/main.jsx
+++ LSC/src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './pages/App.jsx'
 import Study from './pages/study.jsx'
+import Planner from './pages/planner.jsx'
 import Navbar from './navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar/>
-    {/* <App /> */}
-    <Study />
-  </StrictMode>,
+    <App />
+    {/* <Study /> */}
+    {/* <Planner /> */}
+  </StrictMode>
 )
--- LSC/src/navbar.jsx
+++ LSC/src/navbar.jsx
@@ -5,13 +5,15 @@ function Navbar() {
         <>
         <div className='nav-div'>
         <div className="logo-div">
+           <h1>LEARN STUDY CONNECT</h1>
+           <p>Grab Your Future</p>
         </div>
         <div className="navLinks">
-           <a className="navLink">Home </a>
-           <a className="navLink">Study </a>
-           <a className="navLink">Planner </a>
+           <a className="navLink nav-link">Home </a>
+           <a className="navLink nav-link">Study </a>
+           <a className="navLink nav-link">Planner </a>
            <a className="navLink">
-             <img src={userImg} alt="user-symbol"></img>
+             <img src={userImg} alt="user-symbol" className='user-enter-img'></img>
            </a>
         </div>
         </div>
