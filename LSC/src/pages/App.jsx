e4d6d3eca64ba54dda43f260e89ed6983c7c3f38
Thandolwethu Mbokazi <thandwem045@gmail.com>
2025-10-06 18:35:18 +0000

    Added selection and button and styling

--- LSC/src/pages/App.jsx
+++ LSC/src/pages/App.jsx
@@ -10,13 +10,26 @@ function App() {
       <img className="background-img" src={backgroundImage}></img>
       <div className='main-page'>
         <div className='main-header'>
-          <h1>Resources</h1>
-        <p>This is your one stop destination to find study resources.
+          <h1 className='heading'>Resources</h1>
+        <p className='main-subheading'>This is your one stop destination to find study resources.
            From study guides to exam timetables it is all found right here. 
         </p>
         </div>
         
-      </div>
+        <div className='resources-div'>
+          
+          <select name="All" className="selection">
+            <option value="All">All</option>
+            <option value="gradeEight">Grade 8</option>
+            <option value="gradeNine">Grade 9</option>
+            <option value="gradeTen">Grade 10</option>
+            <option value="gradeEleven">Grade 11</option>
+            <option value="gradeTwelve">Grade 12</option>
+          </select>
+
+          <button className='AddItem'>Add Item</button>
+        </div>
+       </div>
      
     </>
   )
