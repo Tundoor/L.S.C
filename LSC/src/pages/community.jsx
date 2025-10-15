dc3288ea2e13bbdddd419ac81ba2e72ecf446874
Thandolwethu Mbokazi <thandwem045@gmail.com>
2025-10-15 08:05:47 +0000

    Styled hero section of community section

--- LSC/src/pages/community.jsx
+++ LSC/src/pages/community.jsx
@@ -1,7 +1,27 @@
+import backgroundImageTwo from "../assets/-James humes.png"
+
 function community() {
-   return (
-      <h1>I'm the community page</h1>
-   )
+    return (
+        <>
+            <img className="background-img"
+                src={backgroundImageTwo}></img>
+            <div className='main-page'>
+                <div className='main-header'>
+                    <h1 className='heading'>Communication</h1>
+                    <p className='main-subheading'>
+                        Looking to make studying and exams just a little easier? Join groups, connect with peers, and be part of communities where students support each other, share resources, and grow together.
+                        Who knows? Joining one study group could be your first step toward a better, more successful future.
+                    </p>
+                </div>
+                 <div className='resources-div'>
+                  <button className='AddConnectBtn'>Add Connection</button>
+                 </div>
+                <div className="TandC'sDiv">
+                  <p className="terms&conditions"></p>
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default community 
+export default community
